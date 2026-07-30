b40a54d3771fcdd8ad4773a39ed34ac7aafc12c2
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-07-30 13:50:26 +0530

    Created async engine and  implemented get_session function

---
+++ backend/src/db/main.py
@@ -0,0 +1,23 @@
+from ..config import Config
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlmodel import SQLModel
+from sqlalchemy.orm import sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+async_engine = create_async_engine(
+    url=Config.DATABASE_URL
+)
+
+async def init_db():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all) 
+        
+async def get_session():
+    Session = sessionmaker(
+        bind=async_engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+    
+    async with Session() as session:
+        yield session
