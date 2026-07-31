7a32ed555db92c7c4eb8a3e1a38a71c2ed963291
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-07-31 12:53:57 +0530

    Implemented models: Conversation, ConversationMember, Message

--- backend/src/db/models.py
+++ backend/src/db/models.py
@@ -2,11 +2,12 @@ from sqlmodel import SQLModel, Field, Column
 import sqlalchemy.dialects.postgresql as pg
 import uuid
 from datetime import datetime
+from typing import Optional
 
 class User(SQLModel, table=True):
     __tablename__="users"
     
-    uid: str = Field(
+    user_uid: uuid.UUID = Field(
         sa_column=Column(
             pg.UUID,
             nullable=False,
@@ -23,3 +24,54 @@ class User(SQLModel, table=True):
     def __repr__(self):
         return f"<Username: {self.username}>" 
     
+class Conversation(SQLModel, table=True):
+    __tablename__ = 'conversation'
+    
+    conv_uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4
+        )
+    )
+    conv_type: str
+    member_count: int = Field(default=0)
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+
+class ConversationMember(SQLModel, table=True):
+    __tablename__='conversation_member'
+    
+    conv_uid: uuid.UUID = Field(
+        foreign_key='conversation.conv_uid',
+        primary_key=True
+    )
+    user_uid: uuid.UUID = Field(
+        foreign_key='users.user_uid',
+        primary_key=True
+    )
+    role: str = "member"
+    joined_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    
+class Message(SQLModel, table=True):
+    __tablename__='messages'
+    
+    message_uid: str = Field(
+        sa_column=Column(
+            pg.UUID,
+            nullable=False,
+            primary_key=True,
+            default=uuid.uuid4
+        )
+    )
+    
+    content: str
+    conv_uid: uuid.UUID = Field(
+        foreign_key='conversation.conv_uid'
+    )
+    sender_uid: uuid.UUID = Field(
+        foreign_key='users.user_uid'
+    )
+    
+    sent_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
