531f7cdb2fa3958100172aa2d3a929cdffa44d20
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-07-31 13:07:50 +0530

    Implemented websokcet connection manager class

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from .chats.routes import chat_router
 from contextlib import asynccontextmanager
 from .db.main import init_db
+from typing import List
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
@@ -18,6 +19,29 @@ app = FastAPI(
     version=version,
     lifespan=life_span
 )
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+        
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        
+    async def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+        
+    async def send_personal_message(self, message:str, websocket: WebSocket):
+        await websocket.send_text(message)
+        
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message) 
+            
+manager = ConnectionManager()
+
+
     
 
 app.include_router(chat_router, prefix=f"/api/{version}/response", tags=['response'])
