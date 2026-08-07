9c4fc7f0a252cc6de74e1fc40bd40561bb18760b
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-08-07 20:28:48 +0530

    Implemented logout button in sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -9,15 +9,19 @@ export function Sidebar() {
     }
     return (
         <div className="h-screen w-[5vw] bg-slate-800 flex flex-col pt-3">
-            <div className="flex flex-col gap-4 items-center p-2 *:text-white *:rounded-full *:p-0.5 *:hover:bg-gray-600 ">
+            <div className="flex flex-col gap-4 items-center p-2 *:text-white *:rounded-full *:p-1 *:hover:bg-gray-600 ">
                 <button className="h-10 w-10">
                     <img src="/chat.png" className="invert-100 w-6 place-self-center-safe"/>
                 </button>
                 <button className="h-10 w-10">
                     <img src="/bot.png" className="invert-100 w-7 place-self-center-safe"/>
                 </button>
-                <button className="h-10 w-10"><img src="/add.png" className="invert-100"/></button>
-                <button onClick={handleLogout}>L</button>
+                <button className="h-10 w-10">
+                    <img src="/add.png" className="invert-100"/>
+                </button>
+                <button onClick={handleLogout}>
+                    <img src='/logout.png' className='h-7 invert-100'/>
+                </button>
             </div>
         </div>
     );
