028063d4de71c5ad50762baa9972269e3fd5757e
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-08-10 17:44:48 +0530

    Created pydantic schema for creating conversation model

--- backend/src/chats/schemas.py
+++ backend/src/chats/schemas.py
@@ -1,8 +1,16 @@
 from pydantic import BaseModel
 import uuid
+from typing import List
 
 class MessageModel(BaseModel):
     content: str
     sender_uid: uuid.UUID
-    conv_uid: uuid.UUID 
+    conv_uid: uuid.UUID     
     
+class ConversationMetadata(BaseModel):
+    conv_type: str
+    member_count: int
+    
+class ConvCreateModel(BaseModel):
+    conversation_metadata: ConversationMetadata
+    users: list[dict]
