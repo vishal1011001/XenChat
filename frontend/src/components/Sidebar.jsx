4bfaa3af89831dcb2c67a4ad340a48d99adf593f
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-08-12 20:58:15 +0530

    Implemented StartNewConversation component in sidedbar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
 import {useNavigate} from 'react-router-dom';
+import {useState} from 'react';
+import { StartNewConversation } from './SidebarComponents/StartNewConversation';
 
 export function Sidebar() {
     const navigate = useNavigate()
@@ -7,6 +9,13 @@ export function Sidebar() {
         localStorage.removeItem('xen_refresh_token');
         navigate('/login');
     }
+
+    const [wantToStartConv, setWantToStartConv] = useState(false);
+
+    const toggleWantNewConversation = () => {
+        setWantToStartConv(!wantToStartConv);
+    }
+
     return (
         <div className="h-screen w-[5vw] bg-slate-800 flex flex-col pt-3">
             <div className="flex flex-col gap-4 items-center p-2 *:text-white *:rounded-full *:p-1 *:hover:bg-gray-600 ">
@@ -16,9 +25,14 @@ export function Sidebar() {
                 <button className="h-10 w-10">
                     <img src="/bot.png" className="invert-100 w-7 place-self-center-safe"/>
                 </button>
-                <button className="h-10 w-10">
+                <button 
+                    onClick={toggleWantNewConversation}
+                    className="h-10 w-10">
                     <img src="/add.png" className="invert-100"/>
                 </button>
+                {wantToStartConv && (
+                    <StartNewConversation />
+                )}
                 <button onClick={handleLogout}>
                     <img src='/logout.png' className='h-7 invert-100'/>
                 </button>
