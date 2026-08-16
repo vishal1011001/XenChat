e179717b1b47ea0b7e66d50c4a50913f35d41d14
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-08-16 16:25:15 +0530

    Implemented autoscroll in Messages (ChatArea)

--- frontend/src/components/ChatAreaComponents/Messages.jsx
+++ frontend/src/components/ChatAreaComponents/Messages.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useRef } from "react";
+
 export function Messages({ messagesToDisplay, currUserUid }) {
+    const messagesEndRef = useRef(null);
+    
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({behavior: "smooth"});
+    }
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messagesToDisplay])
+
     return (
         <div className="p-4 z-1 flex flex-col gap-1.5 **:overflow-y-auto scroll-auto pb-13 justify-end *:rounded-xl **:max-w-2xl **:flex **:flex-col">
             {messagesToDisplay.map((message) => (
@@ -16,6 +28,7 @@ export function Messages({ messagesToDisplay, currUserUid }) {
                     </div>
                 </div>
             ))}
+            <div ref={messagesEndRef}/>
         </div>
     );
 } 
