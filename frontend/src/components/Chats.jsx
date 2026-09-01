b340d68060318ffc66f101bfebc173f55a145671
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-09-01 15:01:50 +0530

    Implemented search among chats functionality in chats component, using search bar child component.

--- frontend/src/components/Chats.jsx
+++ frontend/src/components/Chats.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { FilterButtons } from "./ChatsComponents/FilterButtons";
 import { SearchBar } from "./ChatsComponents/SearchBar";
 
@@ -9,6 +10,8 @@ export function Chats({ conversations, setActiveConvUid, setIsChatOpen }) {
         setIsChatOpen(true);
     }
 
+    const [convToDisplay, setConvToDisplay] = useState(conversations);
+
     return (
         <div className="h-screen w-[35vw] bg-slate-950 flex flex-col">
             <div className="p-4 pl-6 w-[28vw] flex flex-row justify-between items-center">
@@ -16,13 +19,13 @@ export function Chats({ conversations, setActiveConvUid, setIsChatOpen }) {
                 <button className="text-white rounded-full p-1 hover:bg-slate-700"><img src='/more.png' className="h-8 invert-100" /></button>
             </div>
 
-            <SearchBar />
+            <SearchBar conversations={conversations} setConvToDisplay={setConvToDisplay} />
 
             <FilterButtons />
 
             <div className="overflow-y-scroll  scroll scroll-auto scrollbar-none">
                 <div className="p-2 pt-0 flex flex-col gap-2">
-                    {conversations.map((chat) => (
+                    {convToDisplay.map((chat) => (
                         <div key={chat.conv_uid}
                             onClick={() => changeActiveConvUid(chat.conv_uid)}
                             className="flex flex-row pl-3 p-2 hover:bg-slate-900 rounded-xl mr-4">
--- frontend/src/components/ChatsComponents/SearchBar.jsx
+++ frontend/src/components/ChatsComponents/SearchBar.jsx
@@ -1,7 +1,33 @@
-export function SearchBar() {
+import { useEffect, useState } from "react";
+
+export function SearchBar({ conversations, setConvToDisplay }) {
+    const [searchText, setSearchText] = useState('');
+    const changeSearchText = (e) => {
+        setSearchText(e.target.value)
+    }
+    
+    const filteredChats = conversations.filter(
+        convs => convs.member_usernames.some(item => item.includes(searchText))
+    );
+
+    useEffect(() => {
+        if (searchText === '') {
+            setConvToDisplay(conversations);
+        } else {
+            setConvToDisplay(filteredChats);
+        }
+    }, [searchText, conversations]);
+
     return (
         <div className="flex justify-center">
-            <input placeholder="Search chats" className="bg-slate-500 p-2 pl-3 rounded-xl text-white placeholder-white w-[90%] outline-0"/>
+            <input
+                value={searchText}
+                onChange={changeSearchText}
+                placeholder="Search chats" className="bg-slate-700 p-2 pl-3 rounded-l-xl text-white placeholder-white w-[85%] outline-0"
+            />
+            <button onClick={() => setSearchText('')} 
+                className="text-white text-xl bg-slate-700 rounded-r-xl px-3 text-center"
+            >ⓧ</button>
         </div>
     );
 };
