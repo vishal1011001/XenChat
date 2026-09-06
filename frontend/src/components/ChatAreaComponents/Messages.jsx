f0ba9e826ee7d2e1daa82a6b83cf15340198c555
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-09-06 18:05:23 +0530

    Added edit button to msgOptions menu, and implemented changeEditingState function

--- frontend/src/components/ChatAreaComponents/Messages.jsx
+++ frontend/src/components/ChatAreaComponents/Messages.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 
-export function Messages({ sendMessage, activeConvUid, messagesToDisplay, currUserUid }) {
+export function Messages({ sendMessage, activeConvUid, messagesToDisplay, currUserUid, setWantToEdit, setMessageUidToEdit, setText }) {
     const messagesEndRef = useRef(null);
     
     const scrollToBottom = () => {
@@ -31,6 +31,14 @@ export function Messages({ sendMessage, activeConvUid, messagesToDisplay, currUs
         })
     }
 
+    const changeEditingState = (uid, text) => {
+        setWantToEdit(true);
+        setMessageUidToEdit(uid);
+        setText(text);
+
+        setMsgOptionsOpen(false);
+    } 
+
     return (
         <div className="p-4 z-1 flex flex-col gap-1.5 **:overflow-y-auto scroll-auto pb-13 justify-end *:rounded-xl **:max-w-2xl **:flex **:flex-col">
             {messagesToDisplay.map((message) => (
@@ -56,6 +64,10 @@ export function Messages({ sendMessage, activeConvUid, messagesToDisplay, currUs
                                 onClick={deleteMessage}
                                 className=""
                             >Delete</button>
+                            <button
+                                onClick={() => changeEditingState(message.message_uid, message.content)}
+                                className=""
+                            >Edit</button>
                         </div>
                     )}
                 </div>
