822878e57081cfa52d763257b140067f135ec29f
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-09-13 14:11:55 +0530

    Changes in ConnectionManager.py: manager object now in this file, imported in routes and SocketRoutes. Added comments and print statements for explanation and exception print. str() of client_id in connect() function.

--- backend/src/chats/socket/ConnectionManager.py
+++ backend/src/chats/socket/ConnectionManager.py
@@ -1,3 +1,5 @@
+import uuid
+from typing import List, Optional
 from fastapi import WebSocket, WebSocketDisconnect
 
 class SocketConnection:
@@ -17,7 +19,7 @@ class ConnectionManager:
         
     async def connect(self, websocket: WebSocket, client_id: uuid.UUID):
         await websocket.accept()
-        self.active_connections = [c for c in self.active_connections if c.client_id != client_id]
+        self.active_connections = [c for c in self.active_connections if str(c.client_id) != str(client_id)]
         new_conn = SocketConnection(client_id, websocket)
         self.active_connections.append(new_conn)
         
@@ -30,16 +32,23 @@ class ConnectionManager:
     async def send_personal_message(self, message:dict, websocket: WebSocket):
         try:
             await websocket.send_json(message)
-        except Exception:
+        except Exception as e:
+            print("SEND JSON FAILED")
+            print("EXCEPTION TYPE:", type(e))
+            print("EXCEPTION:", repr(e))
             await self.disconnect(websocket)
         
     async def broadcast(self, member_uids: List, message: dict):
         receivers = []
+
+        # Find websocket object and append it to receivers
         for member_uid in member_uids:
             receiver = await self.find_connection(member_uid)
             if receiver:
                 receivers.append(receiver.websocket)
-        
+
+        # Send personal message to all active relevant connections (it's send to only members of relevant conv_uid -> member_uids)
         for connection in receivers:
             await self.send_personal_message(message, connection)
  
+manager = ConnectionManager()
