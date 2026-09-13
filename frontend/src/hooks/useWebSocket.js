d566f148cb695b7541add35362d6c3f46f617dd0
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-09-13 12:14:12 +0530

    Implemented 'create_conv' case in useWebSocket hook to add newly created conversation in conversations list.

--- frontend/src/hooks/useWebSocket.js
+++ frontend/src/hooks/useWebSocket.js
@@ -1,6 +1,6 @@
 import { useRef, useEffect } from "react";
 
-export function useWebSocket(userUid, setMessages) {
+export function useWebSocket(userUid, setConversations, setMessages) {
     const socketRef = useRef(null);
 
     useEffect(() => {
@@ -14,19 +14,25 @@ export function useWebSocket(userUid, setMessages) {
         }
 
         socket.onmessage = (event) => {
-            const message = JSON.parse(event.data);
+            const data = JSON.parse(event.data);
 
-            if (message.req === 'send_msg') {
-                delete message.req;
-                setMessages(prevM => [...prevM, message]);
-            } else if (message.req == 'edit_msg') {
+            if (data.req === 'send_msg') {
+                delete data.req;
+                setMessages(prevM => [...prevM, data]);
+            } else if (data.req == 'edit_msg') {
                 setMessages(prevM => prevM.map(msg => (
-                    msg.message_uid === message.message_uid ? {...msg, content: message.new_content, edited_at: message.edited_at} : msg
-                )
-                ))
-            } else if (message.req === 'delete_msg') {
-                const message_uid = message.message_uid;
+                    msg.message_uid === data.message_uid ? {...msg, content: data.new_content, edited_at: data.edited_at} : msg
+                )))
+            } else if (data.req === 'delete_msg') {
+                const message_uid = data.message_uid;
                 setMessages(prevM => prevM.filter(msg => msg.message_uid !== message_uid));
+            } else if (data.req === 'create_conv') {
+                // Removing req field, and currUsername from member_usernames list
+                delete data.req
+                const currUsername = JSON.parse(localStorage.getItem('xen_user_data')).username;
+                data.member_usernames = data.member_usernames.filter(uname => uname != currUsername);
+                
+                setConversations(prev => [data, ...prev]);
             }
         }
 
